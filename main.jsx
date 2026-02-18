4b25ee153c756695f46da1b8703fc46e020522c6
prasath176 <velprasath176@gmail.com>
2026-02-18 09:58:26 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from "./Home"
+import Post from "./Post"
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+
+    <Routes>
+      <Route path='/'element={<Home/>}> </Route>
+      <Route path='Post'element={<Post/>}> </Route>
+    </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
