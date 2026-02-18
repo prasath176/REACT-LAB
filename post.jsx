0301aa7d286db702550d0c2828905acd71ce093f
prasath176 <velprasath176@gmail.com>
2026-02-18 09:57:21 +0530

    Add files via upload

---
+++ post.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex voluptates
+             expedita iure hic animi quibusdam? Sint ad magnam facere dolores eos laborum 
+             harum, soluta ipsa id corporis, accusamus iusto unde 
+            quaerat maxime, veniam ducimus consequatur asperiores officiis
+             quod eum quo?</p>
+    </div>
+  )
+}
+
+export default Post
